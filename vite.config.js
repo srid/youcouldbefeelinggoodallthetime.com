c9d4e36ab0718a3a31acd72799606d0e8c4d8521
Sridhar Ratnakumar <srid@srid.ca>
2025-05-20 11:18:40 +1000

    use vite

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: '.',
+  server: {
+    open: '/index.html',
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
